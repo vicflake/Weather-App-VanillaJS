686114b1eb0f0017eaa4b3cf1ea482c3f48e54c1
Victoria Moreno <vmoreno@users.noreply.github.com>
2021-01-24 20:14:36 +0000

    Weather API included to show real data, current location button added and geolocation applied to app

--- src/index.js
+++ src/index.js
@@ -26,34 +26,79 @@ if (minutes < 10) {
 let currentDate = document.querySelector(".last-update");
 currentDate.innerHTML = `${day} ${hour}:${minutes}`;
 
-//UPDATE LOCATION WITH CURRENT
+//UPDATE LOCATION WITH INPUT
+
+function search(city){
+  let apiKey = "41398c377c1e7843b58672992d67d0cf";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`
+  axios.get(apiUrl).then(showTemperature);
+}
+
+
 
 function getWeather(event) {
+  let city = document.querySelector(".search-city").value;
+  search(city);
   event.preventDefault();
-  let currentLocation = document.querySelector(".location");
-  let inputCity = document.querySelector(".search-city");
-
-  currentLocation.innerHTML = inputCity.value;
 }
-let city = document.querySelector("form");
-city.addEventListener("submit", getWeather);
 
-// CHANGE UNITS FROM CELSIUS TO FARENHEIT
+
+function showTemperature(response){
+  console.log(response);
+      document.querySelector("h1").innerHTML = response.data.name;
+      let temperature = document.querySelector("div.degrees");
+      let tempCurrent = Math.round(response.data.main.temp);
+      temperature.innerHTML = `${tempCurrent}°C`;
+      
+
+      document.querySelector("#humidity").innerHTML = response.data.main.humidity;
+      document.querySelector("#wind").innerHTML = Math.round(response.data.wind.speed);
+      document.querySelector("#weather-desc").innerHTML = response.data.weather[0].description;
+      document.querySelector("#low-temp").innerHTML = Math.round(response.data.main.temp_min);
+      document.querySelector("#high-temp").innerHTML = Math.round(response.data.main.temp_max);
+
 
 function convertCelsius(event) {
   event.preventDefault();
   let tempCelsius = document.querySelector("div.degrees");
-  tempCelsius.innerHTML = "12°C";
+  tempCelsius.innerHTML = `${tempCurrent}°C`;
 }
-
 let celsiusUnits = document.querySelector("#celsius");
 celsiusUnits.addEventListener("click", convertCelsius);
 
 function convertFarenheit(event) {
   event.preventDefault();
   let tempFarenheit = document.querySelector("div.degrees");
-  tempFarenheit.innerHTML = "53.6°F";
+  let farenheitTemp = `${tempCurrent}` * 9/5 + 32;
+  tempFarenheit.innerHTML = `${farenheitTemp}°F`;
 }
 
 let farenheitUnits = document.querySelector("#farenheit");
 farenheitUnits.addEventListener("click", convertFarenheit);
+
+}
+
+
+function showPosition(position) {
+  let latitude = position.coords.latitude;
+  let longitude = position.coords.longitude;
+  let units = "metric";
+  let apiKey = "41398c377c1e7843b58672992d67d0cf";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=${units}`;
+  axios.get(apiUrl).then(showTemperature);
+}
+function getPosition(event) {
+  event.preventDefault();
+  navigator.geolocation.getCurrentPosition(showPosition);
+}
+let city = document.querySelector("form");
+city.addEventListener("submit", getWeather);
+let currentLocation = document.querySelector(".btn-current");
+currentLocation.addEventListener("click", getPosition);
+
+search("Madrid");
+
+
+
+
+
