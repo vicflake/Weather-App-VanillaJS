ae3b6feb9a83420e121fb3b115ca274654296e32
Victoria Moreno <vmoreno@users.noreply.github.com>
2021-02-13 15:03:48 +0000

    Files revised to have secured https

--- src/index.js
+++ src/index.js
@@ -56,7 +56,7 @@ function showTemperature(response){
       document.querySelector("#weather-desc").innerHTML = response.data.weather[0].description;
       document.querySelector("#low-temp").innerHTML = Math.round(response.data.main.temp_min);
       document.querySelector("#high-temp").innerHTML = Math.round(response.data.main.temp_max);
-      iconElement.setAttribute("src", `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`);
+      iconElement.setAttribute("src", `https://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`);
       iconElement.setAttribute("alt", response.data.weather[0].description);
 
 
